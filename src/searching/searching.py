ca4ba8202856eb5420771b5aa522e35671c8cb51
Brian <lawmen218@yahoo.com>
2020-10-05 23:26:02 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,29 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # If target was not found
+    if start > end:
+        return -1
+# Establishing your start point for divide and concquer
+    middle = start + end // 2
+# If the target happens to be the middle number youre done
+    if arr[middle] == target:
+        return middle
+# if target is less than the middle, call function and start
+# iterating over each number -1, repeaat untl middle = target
+    if target < arr[middle]:
+        return binary_search(arr, target, start, middle - 1)
+    # The other option is if its larget than middle, keep adding
+    # number and compare until middle = target.
+    else:
+        return binary_search(arr, target, middle + 1, end)
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
+# def agnostic_binary_search(arr, target):
     # Your code here
-
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,63 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
+    # The elements are consisting of two arrays combined
     elements = len(arrA) + len(arrB)
+    # The merge array value would be an empty array multipled
+    # by the elements value
     merged_arr = [0] * elements
 
     # Your code here
+    # Range returns a list of integers. We want the intigers
+    # within the elements
 
+    # ?? What determines which array the element/number would be in?
+    for i in range(elements):
+        # If the number isn't arrayA then we are passing in
+        # a number into the merged array and removing a number
+        # from the B array located in the first position
+        if not arrA:
+            merged_arr[i] = arrB.pop(0)
+        # If its not the B array add number to array A and
+        # remove the first value in that array
+        elif not arrB:
+            merged_arr[i] = arrA.pop(0)
+        # otherwise if array A is smaller than B we are
+        # removing first value in array A
+        elif arrA[0] < arrB[0]:
+            merged_arr[i] = arrA.pop(0)
+            # Else its larger than B and we remove B
+        else:
+            merged_arr[i] = arrB.pop(0)
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
     # Your code here
+    # Set up base
+    if len(arr) <= 1:
+        return arr
 
+    #  middle is my piviot
+    middle = len(arr) // 2
+    # left is to the left of the middle/pivot
+    left = arr[:middle]  # why do I need the : after middle?
+    right = arr[middle:]
 
-    return arr
+    # Your calling the merge function and actually just sorting the
+    # data correctly here.
+    return merge(merge_sort(left), merge_sort(right))
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
+
+
+# def merge_in_place(arr, start, mid, end):
     # Your code here
 
-
-def merge_sort_in_place(arr, l, r):
+    # def merge_sort_in_place(arr, l, r):
     # Your code here
-
